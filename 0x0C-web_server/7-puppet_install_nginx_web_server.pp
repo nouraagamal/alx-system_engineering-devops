1d887f0ddb5a5162d482a16068e42cfc287e003c
nouraagamal <nouragamal011@gmail.com>
2024-03-27 15:59:10 -0700

    ..

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,33 @@
+nstall Nginx web server (w/ Puppet)
+
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://github.com/nouraagamal/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+file {'/var/www/html/error_404.html':
+	content => 'Ceci n'est pas une page'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "25i\	 error_page 404 /error_404.html;' /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
